b5b080db828ae15caa1847cbfc5966b324b616cc
Olivia Frederick <newowner@MacBook-Air-2.local>
2026-03-23 13:29:48 -0400

    your message here

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  serverExternalPackages: ["heic2any"],
 };
 
-
-
 export default nextConfig;
-
-
